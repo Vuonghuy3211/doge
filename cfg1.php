92e100a176a60748e0e0b16dd5d13c86643ba0d6
Vuonghuy3211 <59691543+Vuonghuy3211@users.noreply.github.com>
2020-06-03 14:59:21 +0700

    Add files via upload

---
+++ cfg1.php
@@ -0,0 +1,7 @@
+<?php
+// acc id Vuong Mai: 12923
+// acc id Tôi: 8696
+$acc_id = "12923";
+$tk = "1";
+$authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvYWRtaW4uYW1haXRlYW0uY29tXC9mYXJtZXJcL2FwaVwvdjFcL2xvZ2luIiwiaWF0IjoxNTkwNzI3OTQ1LCJleHAiOjE1OTEzMzI3NDUsIm5iZiI6MTU5MDcyNzk0NSwianRpIjoiNU5yR2d4SVZ1TmxnWWpCcSIsInN1YiI6NTU3MzgsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEifQ.mcP3ztnKevTDbJ6CS546thovHNCVm4gRPwKpL7O6AtM";
+?>
